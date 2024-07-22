24e71b1a146fc75383c7dbb920099bc4a53ce161
roberts <roberts365653@gmail.com>
2024-07-22 11:36:41 +0300

    edit

--- gif_search_app/lib/providers/http.dart
+++ gif_search_app/lib/providers/http.dart
@@ -1,7 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// Define a provider for http.Client
 final httpClientProvider = Provider<http.Client>((ref) {
   return http.Client();
 });
